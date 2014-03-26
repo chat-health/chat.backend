c0aba06bc7d074eb267f39d88f7f11d9a32355e9
colinmccann <colindmccann@gmail.com>
2014-03-26 16:06:01 -0400

    and hta had entirely wrong fields

--- models/healthTopicAccessed.js
+++ models/healthTopicAccessed.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 var HealthTopicAccessed = new mongoose.Schema({
   _id: Number,
-  vaccine_id : Number,
-  client_id : Number,
+  topic_id : Number,
   visit_id : Number,
-  date : String
+  hh_id : Number,
+  topic_name : String,
+  start_time : String,
+  end_time : String
 }, { versionKey: false, collection: 'health_topics_accessed' });
 // Hiding __v - version key http://stackoverflow.com/questions/13699784/mongoose-v-property-hide
 module.exports = mongoose.model('health_topic_accessed', HealthTopicAccessed);
