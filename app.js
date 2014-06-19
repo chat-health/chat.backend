91727589fd89a5bfb368ca06d0b92313581f762e
Armin Krauss <armin.krauss@utoronto.ca>
2014-06-19 16:15:28 -0400

    Helpers to know how to turn auth on and off

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ var auth = require("./auth");
 app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
+  // to disable security disable these lines
   app.use(express.session({secret: "90ndsj9dfdsf"}));
   app.use("/auth",auth.verifyGoogleToken);
   app.use(auth.verifyClientToken);
+  // to disable security disable these lines
   app.use(app.router);
   // app.set('view engine', 'jade');
   // app.use(express.static(__dirname + '/public'));
