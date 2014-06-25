ec4b0f449d29786e2c1eec74d72eb2e8f4135b72
colinmccann <colindmccann@gmail.com>
2014-06-25 17:15:44 -0400

    new service model with zulu

--- models/service.js
+++ models/service.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var Service = new mongoose.Schema({
   _id : Number,
-  name : String,
+  en_name : String,
+  zu_name : String,
   type : String,
   role : String,
   instructions : String,
